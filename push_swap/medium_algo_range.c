05f24d285e772c9be19fb5138cfd72decacc5d3c
Shaun Liew Khye Yang <161272658+MrAFirm@users.noreply.github.com>
2026-09-10 16:14:28 +0800

    WORKING (But not efficient) RANGE ALGO
    Starts for the biggest range and works it's way down, pushing a range of nums to b and get sorted in descending order, then pa to stack a top to make it ascending order and repeat.

--- push_swap/medium_algo_range.c
+++ push_swap/medium_algo_range.c
@@ -6,7 +6,7 @@
 /*   By: likhye-y <likhye-y@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/04 22:15:32 by likhye-y          #+#    #+#             */
-/*   Updated: 2026/09/09 17:19:29 by likhye-y         ###   ########.fr       */
+/*   Updated: 2026/09/10 16:07:14 by likhye-y         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,6 @@ void	range_sort_algo(t_stack_a *stack_a, t_stack_b *stack_b)
 	t_list	*head;
 	int		size;
 	int		i;
-	// int		j;
 	int		range_start;
 	int		range_end;
 	int		iter;
@@ -32,40 +31,45 @@ void	range_sort_algo(t_stack_a *stack_a, t_stack_b *stack_b)
 	head = stack_a->top;
 	size = (int)ft_lstsize(head);
 	i = 0;
-	// j = 0;
 	blocksize = block_size(stack_a);
 	arr = coords_compress(stack_a);
-	range_start = 0;
+	range_start = size - blocksize;
 	iter = range_start;
-	range_end = blocksize - 1;
-	// while ((unsigned int)i < ft_lstsize(head))
-	// {
-	// 	printf("Current: %d\nIndex: %d\n", stack_a->top->content, stack_a->top->index);
-	// 	i++;
-	// 	stack_a->top = stack_a->top->next;
-	// }
-	while (range_end < size)
+	range_end = size - 1;
+
+	while (range_start >= 0)
 	{
-		// j = 0;
 		while (iter <= range_end)
 		{
-			// printf("A top: %p\n", (void *)stack_a->top);
-			if (stack_a->top->index >= range_start && stack_a->top->index <= range_end)
+			if (range_end == 0 && range_start == 0 && stack_a->top->index == 0 && !stack_a->top->next)
 			{
 				push_b(stack_a, stack_b);
 				iter++;
 			}
 			else
-				rotate_a(stack_a);
+			{
+				if (stack_a->top->index >= range_start && stack_a->top->index <= range_end)
+				{
+					push_b(stack_a, stack_b);
+					iter++;
+				}
+				else
+					rotate_a(stack_a);
+			}
 		}
 		if (stack_b->top)
 			de_bubble_sort(stack_a, stack_b);
-		if (range_end != size - 1)
-		{
-			range_start += blocksize;
-			iter = range_start;
-			range_end += blocksize;
-		}
+		if (range_start == 0)
+			return ;
+		if (range_start == 0 && range_end == 0)
+			return ;
+		range_start -= blocksize;
+		if (range_start < 0)
+			range_start = 0;
+		iter = range_start;
+		range_end -= blocksize;
+		if (range_end < 0)
+			range_end = 0;
 	}
 }
 
@@ -78,6 +82,7 @@ static void	de_bubble_sort(t_stack_a *stack_a, t_stack_b *stack_b)
 
 	i = 0;
 	head = stack_b->top;
+
 	while (i < ft_lstsize(head))
 	{
 		j = 0;
@@ -88,7 +93,7 @@ static void	de_bubble_sort(t_stack_a *stack_a, t_stack_b *stack_b)
 			rotate_b(stack_b);
 			j++;
 		}
-		while (j < ft_lstsize(head))
+		while (j < ft_lstsize(stack_b->top))
 		{
 			rotate_b(stack_b);
 			j++;
@@ -96,9 +101,7 @@ static void	de_bubble_sort(t_stack_a *stack_a, t_stack_b *stack_b)
 		i++;
 	}
 	while (stack_b->top)
-	{
 		push_a(stack_a, stack_b);
-	}
 }
 
 static int	*coords_compress(t_stack_a *stack_a)
@@ -195,7 +198,21 @@ int main()
 	t_list	*new = ft_lstnew(2);
 	t_list	*new2 = ft_lstnew(3);
 	t_list	*new3 = ft_lstnew(1);
+	t_list	*new4 = ft_lstnew(4);
+	t_list	*new5 = ft_lstnew(5);
+	t_list	*new6 = ft_lstnew(9);
+	t_list	*new7 = ft_lstnew(15);
+	t_list	*new8 = ft_lstnew(22);
+	t_list	*new9 = ft_lstnew(109);
+	t_list	*new10 = ft_lstnew(8);
 	
+	ft_lstadd_front(&head, new10);
+	ft_lstadd_front(&head, new9);
+	ft_lstadd_front(&head, new8);
+	ft_lstadd_front(&head, new7);
+	ft_lstadd_front(&head, new6);
+	ft_lstadd_front(&head, new5);
+	ft_lstadd_front(&head, new4);
 	ft_lstadd_front(&head, new3);
 	ft_lstadd_front(&head, new2);
 	ft_lstadd_front(&head, new);
@@ -211,6 +228,15 @@ int main()
 	}
 	// bubble_sort(&stack_a);
 	range_sort_algo(&stack_a, &stack_b);
+
+	current = stack_a.top;
+	c = 0;
+	while (current)
+	{
+		printf("%d: %i\n", c, current->content);
+		current = current->next;
+		c++;
+	}
 
 	// while ((unsigned int)i < ft_lstsize(stack_a.top))
 	// {
@@ -229,6 +255,11 @@ int main()
 	// 	c++;
 	// }
 
-	
+	// while ((unsigned int)i < ft_lstsize(head))
+	// {
+	// 	printf("Current: %d\nIndex: %d\n", stack_a->top->content, stack_a->top->index);
+	// 	i++;
+	// 	stack_a->top = stack_a->top->next;
+	// }
 	
 }
